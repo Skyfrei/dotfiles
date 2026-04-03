d3dcc85bf9d99b041058d1fb4df8a5f7ad87b4f1
Skyfrei <tarkaklavio@gmail.com>
2026-04-03 14:34:21 +0200

    using fish instead of bash now + wallpapers added

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+set -gx VIMRUNTIME "$HOME/code/neovim/runtime"
+set fish_greeting
+
+### Path Update ###
+# fish_add_path is smart; it checks if the path exists and avoids duplicates
+fish_add_path "$HOME/code/neovim/build/bin"
+
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec start-hyprland
+    end
+end
+
+if status is-interactive
+    if test -z "$TMUX" -a -n "$WAYLAND_DISPLAY"
+        if type -q tmux
+            exec tmux new-session -A -s $USER
+        end
+    end
+end
+
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+
+function fish_prompt
+    set -l user_color (set_color green)
+    set -l normal (set_color normal)
+    echo -n "["$user_color$USER$normal"@(hostname) "(prompt_pwd)"]\$ "
+end
